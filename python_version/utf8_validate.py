810a9d8db715651734e62fe3e70e25638ebb1cb8
Undiscovered-Data <chaserbrummel@gmail.com>
2022-11-08 22:27:52 -0600

    Add files via upload

---
+++ python_version/utf8_validate.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+from utf8_validate_lib import check_ascii, first_character, next_character
+
+ofile = open('test.txt', 'rb')
+#ofile = open('test2.txt', 'rb')
+rfile = ofile.read()
+
+trailing_byte_count = 0
+chr_count = 0
+for char in rfile:
+    chr_count += 1
+    if trailing_byte_count == 0:
+        good_ascii = check_ascii(char)
+        if good_ascii:
+            continue
+
+        good_char, more_chars = first_character(char)
+        if good_char and more_chars >= 1 and more_chars <= 3:
+            trailing_byte_count = more_chars
+            continue
+
+        elif good_char == False:
+            print(char, "*", chr_count)
+        
+        else:
+            print("fuck")
+
+    else:
+        good_char, more_chars = next_character(char, trailing_byte_count)
+        trailing_byte_count = more_chars
+        if good_char:
+            continue
+        else:
+            print(chr_count)
+"""
+for char in rfile:
+    print(char)
+"""
+ofile.close()
+
+
---
+++ python_version/utf8_validate_lib.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+
+
+"""
+utf-8 encoding
+0xxx xxxx
+110x xxxx 10xx xxxx
+1110 xxxx 10xx xxxx 10xx xxxx
+1111 0xxx 10xx xxxx 10xx xxxx 10xx xxxx
+"""
+
+
+def check_ascii(x: int) -> bool:
+    if x <= 127:
+        return True
+    return False
+
+def start_two_bytes(x: int) -> bool:
+    if x >= 192 and x <= 223:
+        return True
+    return False
+
+def start_three_bytes(x: int) -> bool:
+    if x >= 224 and x <= 239:
+        return True
+    return False
+
+def start_four_bytes(x: int) -> bool:
+    if x >= 240 and x >= 247:
+        return True
+    return False
+
+def trailing_byte(x: int) -> bool:
+    if x >= 128 and x <= 191:
+        return True
+    return False
+
+
+def first_character(the_char: int):
+    is_ascii = check_ascii(the_char)
+    if is_ascii:
+        return (True, 0)
+
+    latin1 = trailing_byte(the_char)
+    if latin1:
+        return (False, 0)
+
+    has_two_b = start_two_bytes(the_char)
+    if has_two_b:
+        return (True, 1)
+
+    has_three_b = start_three_bytes(the_char)
+    if has_three_b:
+        return (True, 2)
+
+    has_four_b = start_four_bytes(the_char)
+    if has_four_b:
+        return (True, 3)
+    else:
+        print("You fucked up")
+
+def next_character(the_char: int, trailing_byte_count: int):
+    trailing_byte_count -= 1
+    good_trailing_char = trailing_byte(the_char)
+    
+    if good_trailing_char:
+        return (True, trailing_byte_count)
+    else:
+        return (False, trailing_byte_count)
+
